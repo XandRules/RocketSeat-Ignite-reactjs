05a4ffdd05acd25f6f3e172bc7fdfa6c17e2372c
Alexandre <alexandresilva58@gmail.com>
2021-09-19 23:21:49 -0300

    (chore) configurado webpack com sourceMap

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "index.jsx"),
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
 
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
 
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
+  },
+
+  devServer: {
+    static: path.resolve(__dirname, 'public') 
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
+      template: path.resolve(__dirname, 'public', 'index.html'),
     }),
   ],
   module: {
@@ -24,7 +29,7 @@ module.exports = {
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
     ],
   },
