f032086c5d8fcefdce5d5ceecbfc646ff24bc9e2
Alexandre <alexandresilva58@gmail.com>
2021-09-26 18:49:54 -0300

    (chore) criado component repository-list

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,27 @@
+
+export function RepositoryList(){
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            
+            <ul>
+                <li>
+                    <strong>Título</strong>
+                    <p>Descrição do repositório</p>
+                    <a href="">Link para o repositório</a>
+                </li>
+                <li>
+                    <strong>Título</strong>
+                    <p>Descrição do repositório</p>
+                    <a href="">Link para o repositório</a>
+                </li>
+                <li>
+                    <strong>Título</strong>
+                    <p>Descrição do repositório</p>
+                    <a href="">Link para o repositório</a>
+                </li>
+            </ul>
+            
+        </section>
+    );
+}
